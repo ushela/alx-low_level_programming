50024c8b3de5652ffe19343b71d79441e5e499f0
Ekunke Clement <ushie5797@gmail.com>
2022-09-08 21:31:40 -0700

    low level question 1-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+nclude <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * Description: puts "Programming is like building a multilingual puzzle"
+ *    * Return: 0
+ *     */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
